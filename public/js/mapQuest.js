2eb12b421317328f4079a9483d56038456f32a6e
Raymond Ibrahim <rribrahi@ucsd.edu>
2021-02-25 10:46:20 -0800

    added map

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
+L.mapquest.key = 'O3UaBILtZZ4sZAg5nTZdioDCkFAsnLm8';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+  //zoomControl: false
+});
 	// add your code here
+L.marker([32.88, -118]).addTo(map);
 }
